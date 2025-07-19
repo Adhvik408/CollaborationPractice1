0d6c8ed1cc5a32c4c8f7f824dc4ba0ed8fef15ab
Adhvik408 <68874264+Adhvik408@users.noreply.github.com>
2025-07-18 21:09:08 -0400

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,23 @@
+class Calculator:
+    def multiply(factor1: float, factor2: float) -> float:
+        pass
+
+    def divide(dividend: float, divisor: float) -> float:
+        pass
+    
+    def add(number1: float, number2: float) -> float:
+        pass
+    
+    def subtract(minuend: float, subtrahend: float) -> float:
+        pass
+    
+    def power(base: float, exponent: int) -> float:
+        return base ** exponent
+
+
+
+#assert Calculator.power(2.2, 3) == 10.648
+#assert Calculator.multiply(2.4, 3.3) == 7.92
+#assert Calculator.divide(6.6, 2.2) == 3
+#assert Calculator.add(2.2, 3.3) == 5.5
+#assert Calculator.subtract(5.5, 2.2) == 3.3
